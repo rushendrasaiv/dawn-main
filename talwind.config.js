4fb5c237e2137559e1d966b9ba00eff6fc605a8f
RSV2255 <rushivutukuri@gmail.com>
2025-05-28 13:42:17 +0530

    first commit

---
+++ talwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './**/*.liquid', // scan all liquid files
+    './sections/**/*.liquid',
+    './blocks/**/*.liquid',
+    './snippets/**/*.liquid',
+    './templates/**/*.liquid',
+    './layout/**/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
